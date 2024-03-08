114dbeb5fe9722cd0958bcb8aa2dc06c53283a42
yas3jb <yasmu213@gmail.com>
2024-03-08 17:58:06 -0500

    fetchCustomers, fetchResturants are required

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const {
   createTables,
   createCustomer,
   createResturant,
+  fetchCustomers,
+  fetchResturants,
 } = require("./db");
 
 const init = async () => {
@@ -14,16 +16,19 @@ const init = async () => {
   // Promise.all to create 3 Customers and 4 Resturants
   const [Edwin, Liz, Kavin, Rome_Cafe, NYC_Cafe, LA_Cafe, Paris_Cafe] =
     await Promise.all([
-      createUser("Edwin"),
-      createUser("Liz"),
-      createUser("Kavin"),
-      createPlace("Rome_Cafe"),
-      createPlace("NYC_Cafe"),
-      createPlace("LA_Cafe"),
-      createPlace("Paris_Cafe"),
+      createCustomer("Edwin"),
+      createCustomer("Liz"),
+      createCustomer("Kavin"),
+      createResturant("Rome_Cafe"),
+      createResturant("NYC_Cafe"),
+      createResturant("LA_Cafe"),
+      createResturant("Paris_Cafe"),
     ]);
   console.log(`Edwin has an id of ${Edwin.id}`);
+  console.log(`Kavin has an id of ${Liz.id}`);
   console.log(`Kavin has an id of ${Kavin.id}`);
+  console.log(await fetchCustomers());
+  console.log(await fetchResturants());
 };
 
 init();
