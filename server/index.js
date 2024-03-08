812ccfd700eab4d1f656b37d364b9477cbc81970
yas3jb <yasmu213@gmail.com>
2024-03-08 17:42:30 -0500

    Promise.all to create 3 Customers and 4 Resturants

--- server/index.js
+++ server/index.js
@@ -10,6 +10,20 @@ const init = async () => {
   console.log("connected to database");
   await createTables();
   console.log("tables created");
+
+  // Promise.all to create 3 Customers and 4 Resturants
+  const [Edwin, Liz, Kavin, Rome_Cafe, NYC_Cafe, LA_Cafe, Paris_Cafe] =
+    await Promise.all([
+      createUser("Edwin"),
+      createUser("Liz"),
+      createUser("Kavin"),
+      createPlace("Rome_Cafe"),
+      createPlace("NYC_Cafe"),
+      createPlace("LA_Cafe"),
+      createPlace("Paris_Cafe"),
+    ]);
+  console.log(`Edwin has an id of ${Edwin.id}`);
+  console.log(`Kavin has an id of ${Kavin.id}`);
 };
 
 init();
