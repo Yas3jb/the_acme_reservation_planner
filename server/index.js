e03b7e322b69adef69510696f0e0d2eb7c84790e
yas3jb <yasmu213@gmail.com>
2024-03-08 19:21:40 -0500

    createReserverstion added

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ const {
   createResturant,
   fetchCustomers,
   fetchResturants,
+  createReservation,
+  fetchReservations,
 } = require("./db");
 
 const init = async () => {
@@ -13,12 +15,13 @@ const init = async () => {
   await createTables();
   console.log("tables created");
 
-  // Promise.all to create 3 Customers and 4 Resturants
-  const [Edwin, Liz, Kavin, Rome_Cafe, NYC_Cafe, LA_Cafe, Paris_Cafe] =
+  // Promise.all to create 4 Customers and 4 Resturants
+  const [Edwin, Liz, Kavin, Yasin, Rome_Cafe, NYC_Cafe, LA_Cafe, Paris_Cafe] =
     await Promise.all([
       createCustomer("Edwin"),
       createCustomer("Liz"),
       createCustomer("Kavin"),
+      createCustomer("Yasin"),
       createResturant("Rome_Cafe"),
       createResturant("NYC_Cafe"),
       createResturant("LA_Cafe"),
@@ -29,6 +32,35 @@ const init = async () => {
   console.log(`Kavin has an id of ${Kavin.id}`);
   console.log(await fetchCustomers());
   console.log(await fetchResturants());
+
+  // Promise.all to create 4 Reservations
+  await Promise.all([
+    createReservation({
+      customer_id: Edwin.id,
+      resturant_id: LA_Cafe.id,
+      party_count: 4,
+      reservation_date: "04/01/2024",
+    }),
+    createReservation({
+      customer_id: Kavin.id,
+      resturant_id: Rome_Cafe.id,
+      party_count: 3,
+      reservation_date: "03/06/2024",
+    }),
+    createReservation({
+      customer_id: Liz.id,
+      resturant_id: Paris_Cafe.id,
+      party_count: 7,
+      reservation_date: "01/13/2024",
+    }),
+    createReservation({
+      customer_id: Yasin.id,
+      resturant_id: LA_Cafe.id,
+      party_count: 5,
+      reservation_date: "08/08/2024",
+    }),
+  ]);
+  console.log(await fetchReservations());
 };
 
 init();
